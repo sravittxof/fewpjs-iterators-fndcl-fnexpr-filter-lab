a664dbe4edb542d9e40c1c82641c525f586befe1
sravittxof <travis.fox90@gmail.come>
2021-04-26 21:07:09 -0400

    commit tree fe987055817a201a2a75f4168c343cc51d035160

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solution here
+function findMatching(drivers, string){
+    const matches = drivers.filter(name => {
+        return name.toUpperCase() === string.toUpperCase();
+    });
+    return matches;
+}
+
+function fuzzyMatch(drivers, string){
+    const matches = drivers.filter(name => {
+        return name.slice(0, string.length) === string;
+    })
+    return matches;
+}
+
+function matchName(drivers, string){
+    const matches = drivers.filter(driver => {
+        return driver['name'] === string;
+    })
+    return matches;
+}
